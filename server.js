a7a71ff7eec8639529798e50cfd3af77479282b7
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-18 10:19:37 -0600

    add feedback

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//great use of `` with h1, br, and a tags
+//great job connecting / to /:number_of_bottles
+//I would recommend working into the bonus, little bugs in the code so you can get more practice adding logic.
+//I put my solution at the end of the file if you are interested, but if you want to try later, don't look!
 
 app.get("/", (req, res) => {
   res.send(`
@@ -21,6 +25,38 @@ app.get("/:number_of_bottles", (req, res) => {
     }
 })
 
+
+//little bugs in the code
+app.get('/:number', (req, res) => {
+  let number = parseInt(req.params.number)
+  let next = number - 1
+  if (number === randomNum) {
+      number += 20
+      console.log(number, 'this is number')
+      console.log(next, 'this is next')
+      randomNum = Math.floor(Math.random() * (0, 99) + 1)
+      console.log(randomNum)
+      res.send(`
+          <h1>${number} little bugs in the code.</h1>
+          <a href='/${number - 1}'>take one down, pass it around...</a>
+      `)
+
+  } else if (number > 0) {
+      res.send(`
+          <h1>${number} little bugs in the code.</h1>
+          <a href=/${next}>take one down, pass it around...</a>
+      `)
+  } else if (number === 0)res.send(`
+      <h1>All the beer is gone!</h1>
+      <a href='/'>Start Over</a>
+      `)
+})
+
+
+
+
+
 app.listen(4000, () => {
   console.log('listening');
 });
+
